8793cd0f6d3cb350049ceee2cb772cd4800e75ad
EI-Flores <ing.flores.rivera@outlook.com>
2021-11-23 20:58:30 -0600

    update main & README

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# pip install pyttsx3
-# pip install SpeechRecognition
-# pip install PyAudio
-#if your installation send "Error" 
-# OPTION 1 
-# chk  https://stackoverflow.com/questions/52283840/i-cant-install-pyaudio-on-windows-how-to-solve-error-microsoft-visual-c-14
-# OPTION 2 (Easier)
-# First run your IDE or CMD as Administrator and run the following:
-# pip install pipwin
-# pipwin install pyaudio
-# pip install wikipedia
-# pip install pyautogui
-# pip install psutil
-# pip install pyjokes
-
 import pyttsx3 
 import datetime
 import speech_recognition as sr 
